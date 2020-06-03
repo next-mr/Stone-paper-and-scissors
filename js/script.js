0b80e9d63795cdd3b6f895d7685802702d444843
Marcin Rudzinski <marcin.rudzinski@next-it.pl>
2020-06-03 23:19:49 +0200

    Player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+} else
+if(playerInput == '2'){
+    playerMove = 'papier';
+} else
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 let computerMove = 'nieznany ruch';
